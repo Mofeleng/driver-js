999358f22e54f463779189ab6fd76d4d8b64584b
Mofeleng <mofelengmokgope@gmail.com>
2026-03-20 16:54:35 +0200

    Implemented car controls and physics

--- classes/car.js
+++ classes/car.js
@@ -6,17 +6,63 @@ class Car {
         this.width = width;
         this.height = height;
 
+        this.velocity = 0;
+        this.acceleration = 0.2;
+        this.maxVelocity = 3;
+        this.friction = 0.05;
+
+        this.angle = 0;
+
         this.controls = new Controls();
+    }
+    #moveCar() {
+        if (this.controls.forward) {
+            this.velocity += this.acceleration;
+        }
+        if (this.controls.reverse) {
+            this.velocity -= this.acceleration;
+        }
+
+        if (this.velocity != 0) {
+            const flip = this.velocity > 0 ? 1 : -1;
+
+            if (this.controls.right) {
+                this.angle -= 0.03 * flip;
+            } 
+            if (this.controls.left) {
+                this.angle += 0.03 * flip;
+            }
+        }
+
+        if (this.velocity > this.maxVelocity) { this.velocity =  this.maxVelocity; }
+        if (this.velocity < -this.maxVelocity) { this.velocity = -this.maxVelocity/2; }
+
+        if (this.velocity > 0) { this.velocity -= this.friction; } 
+        if (this.velocity < 0) { this.velocity += this.friction; } 
+        if (Math.abs(this.velocity) < this.friction) { this.velocity = 0; }
+
+        this.x -= Math.sin(this.angle)*this.velocity;
+        this.y -= Math.cos(this.angle)*this.velocity;
+    }
+    
+    update() {
+        this.#moveCar();
     }
 
     draw(ctx) {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(-this.angle);
+
         ctx.beginPath();
         ctx.rect(
-            this.x - this.width/2,
-            this.y - this.height/2,
+            -this.width/2,
+            -this.height/2,
             this.width,
             this.height
         );
         ctx.fill();
+
+        ctx.restore();
     }
 }
